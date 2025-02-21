8abb6e66558bce17610b25f608fed145ef892fdc
Jokeuuur <222cook222@gmail.com>
2025-02-21 17:48:34 +0100

    fix conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  has_many :anecdotes
-=======
   has_many :anecdotes, dependent: :destroy
->>>>>>> c0296e4f339d2725c71ea54cfbd41afb62c6ca2d
 
   def to_s
     pseudonym
